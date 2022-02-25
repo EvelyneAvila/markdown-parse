325ed61c3be830d154278368f7eeb3a7326fa984
EvelyneAvila <emavila@ucsd.edu>
2022-02-25 09:12:05 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -102,12 +102,12 @@ public class MarkdownParseTest {
         assertEquals("[a.com, a.com(()), example.com]", links.toString());
     }
 
-    /*
+    
     @Test
     public void testSnippet3() throws IOException {
         Path fileName = Path.of("snippet3.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         assertEquals("[https://ucsd-cse15l-w22.github.io/]", links.toString());
-    }*/
+    }
 }
